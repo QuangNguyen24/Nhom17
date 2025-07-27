75567420dca8625a98640685eae8717b70c9a84e
Quang Nguyen <quang02102004@gmail.com>
2025-07-28 02:59:32 +0700

    fix: load model and update code

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'af06b4b8d9f4695ad5dd010627ab5f2dc8e1ce14',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2d4c98125705489c12a7e884ee06beefbc8898a9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'af06b4b8d9f4695ad5dd010627ab5f2dc8e1ce14',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2d4c98125705489c12a7e884ee06beefbc8898a9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
